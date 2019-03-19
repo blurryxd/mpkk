1634ecb5a5a8fdf38bf539a2c90308947c92a1c5
blurryxd <20337853+blurryxd@users.noreply.github.com>
2019-03-19 15:22:01 +0200

    upload

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import './App.css';
+import PicArray from './components/PicArray';
+
+class App extends Component {
+  state = {
+    picArray: [ ],
+  };
+
+  componentDidMount() {
+    fetch( 'test.json').then((response) => {
+      return response.json();
+    }).then( (json) => {
+      console.log(json);
+      this.setState({picArray:json});
+    })
+  }
+
+  render() {
+    return (
+        <table>
+          <tbody>
+          <PicArray picArray={this.state.picArray}/>
+          </tbody>
+        </table>
+    );
+  }
+}
+
+export default App;
