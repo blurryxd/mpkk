9d3ba5a0f5fbec67957760e4bca80d6d731523d5
blurryxd <20337853+blurryxd@users.noreply.github.com>
2019-03-19 17:53:19 +0200

    update working

--- my-app/src/components/PicArray.js
+++ my-app/src/components/PicArray.js
@@ -5,14 +5,14 @@ class PicArray extends Component {
     return this.props.picArray.map((picArr, i) => (
     <tr key={i}>
           <td>
-            <img src={'http://media.mw.metropolia.fi/wbma/media/' + picArr.thumbnails.w160} alt={picArr.title} />
+            <img src={'http://media.mw.metropolia.fi/wbma/uploads/' + picArr.thumbnails.w160} alt={picArr.title} />
           </td>
           <td>
             <h3>{picArr.title}</h3>
             <p>{picArr.description}</p>
           </td>
           <td>
-            <a href={picArr.filename}>View</a>
+            <a href={'http://media.mw.metropolia.fi/wbma/uploads/' + picArr.filename}>View</a>
           </td>
         </tr>
 
